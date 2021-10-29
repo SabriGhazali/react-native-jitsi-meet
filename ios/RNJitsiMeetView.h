dec986976c818a5b7d7b49b0c937caae5eb7db8e
SabriGhazali <51703522+SabriGhazali@users.noreply.github.com>
2021-10-29 14:47:16 +0100

    Update RNJitsiMeetView.h

--- ios/RNJitsiMeetView.h
+++ ios/RNJitsiMeetView.h
@@ -6,5 +6,6 @@
 @property (nonatomic, copy) RCTBubblingEventBlock onConferenceJoined;
 @property (nonatomic, copy) RCTBubblingEventBlock onConferenceTerminated;
 @property (nonatomic, copy) RCTBubblingEventBlock onConferenceWillJoin;
+@property (nonatomic, copy) RCTBubblingEventBlock onParticipantLeft;
 @property (nonatomic, copy) RCTBubblingEventBlock onEnteredPip;
 @end
